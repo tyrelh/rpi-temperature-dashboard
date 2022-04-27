42d6a425016dab7702d76a877646a9bdeed604c5
tyrelh <tyrel.hiebert@gmail.com>
2022-04-27 08:58:57 -0700

    prefix assets to serve them from superflux.dev

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: ''
+  exportPathMap: function () {
+    return {
+      "/": { page: "/"}
+    }
+  },
+  assetPrefix: !debug ? 'https://superflux.dev/rpi-temperature-dashboard/' : ''
 }
 
 module.exports = nextConfig
