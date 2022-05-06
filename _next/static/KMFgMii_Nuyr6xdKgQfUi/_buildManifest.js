ac3c5d5b6880950098a6ca998bd6d0f154c2ec23
tyrelh <tyrel.hiebert@gmail.com>
2022-05-05 23:15:02 -0700

    Updates

---
+++ _next/static/KMFgMii_Nuyr6xdKgQfUi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/860-5797d54d04d87d2d.js","static/chunks/pages/index-50d8374e105b370c.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
